35a7039cabe48aa6623bf8ac91d22b7ef2779605
Chris Cole <cccole3@gmail.com>
2020-08-10 20:08:40 -0500

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# [Christopher & Michelle's Wedding Website](https://cccole3.github.io/cole-wedding)
-
-Our wedding website is written in [TypeScript](http://www.typescriptlang.org) and
-uses [React](https://facebook.github.io/react/).
-
-![Website screenshot - Windows](https://raw.githubusercontent.com/cccole3/cole-wedding/master/website-screenshot.png)
---
+++ precache-manifest.6907eff9b8ab29c75b561cf27f1d130d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ddb91260b9aea396de7c9ba32b16650",
+    "url": "/cole-wedding/index.html"
+  },
+  {
+    "revision": "5d9f276c061f052eaf1a",
+    "url": "/cole-wedding/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bcd531b857647c4346d5",
+    "url": "/cole-wedding/static/js/2.2de5e93c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cole-wedding/static/js/2.2de5e93c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9f276c061f052eaf1a",
+    "url": "/cole-wedding/static/js/main.288be3ea.chunk.js"
+  },
+  {
+    "revision": "46326afe7f6f632957af",
+    "url": "/cole-wedding/static/js/runtime-main.c8ca7054.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cole-wedding/static/media/logo.5d5d9eef.svg"
+  }
+]);
