9cff1666a22ffe851ff9127f64aeb7ec5bed52fd
Chris Cole <cccole3@gmail.com>
2020-08-11 10:37:15 -0500

    Updates

---
+++ precache-manifest.41a4e5539a3abdee6a484750c28ffcba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4930380239304ba18e1ffe03ee635b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed672a5551955dba604f",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4f4e70961e16148d7963",
+    "url": "/static/css/main.d01d5591.chunk.css"
+  },
+  {
+    "revision": "ed672a5551955dba604f",
+    "url": "/static/js/2.8bd434c2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8bd434c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4e70961e16148d7963",
+    "url": "/static/js/main.a74d0c1b.chunk.js"
+  },
+  {
+    "revision": "f0f4b69a6b156efa9f9d",
+    "url": "/static/js/runtime-main.1992d34f.js"
+  },
+  {
+    "revision": "45f8852b8dac1c23c325a1c33571aa51",
+    "url": "/static/media/BRUSHSCI.45f8852b.ttf"
+  },
+  {
+    "revision": "c1c92d0c804d46cb62814e7389725082",
+    "url": "/static/media/GreatVibes-Regular.c1c92d0c.ttf"
+  },
+  {
+    "revision": "4dacbda805dca7c405e17f0d1deb094d",
+    "url": "/static/media/logo.4dacbda8.svg"
+  }
+]);
