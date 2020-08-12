de7333399f8c1d47508a9c8b52ad82baf40ad75f
Chris Cole <cccole3@gmail.com>
2020-08-11 19:54:26 -0500

    adding cname and registry updates

--- src/Pages/Registry/Registry.tsx
+++ src/Pages/Registry/Registry.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
-import Iframe from 'react-iframe'
+// import Iframe from 'react-iframe'
 import Container from 'react-bootstrap/Container'
-import './Registry.css'
+
+interface SelectProtected {
+  readonly inputElement: HTMLElement
+}
 
 export class Registry extends React.Component {
+  componentDidMount() {
+    const script = document.createElement('script')
+    script.id = 'zola-wjs'
+    script.async = true
+    script.src = 'https://widget.zola.com/js/widget.js'
+    const zola = document.getElementById('zola')
+
+    zola!.appendChild(script)
+  }
   render() {
     return (
-      <Container fluid={true}>
-        <Iframe url="https://www.zola.com/registry/christopherandmichellemay22"></Iframe>
+      <Container fluid={true} id="zola">
+        <a
+          className="zola-registry-embed"
+          href="https://www.zola.com/registry/christopherandmichellemay22"
+          data-registry-key="christopherandmichelle22"
+        >
+          Our Zola Wedding Registry
+        </a>
       </Container>
     )
   }
